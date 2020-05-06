4f37f28e0ece756428e50ee3a4ba33ecf8fe2223
pschild <philippe.schild@googlemail.com>
2020-05-06 07:23:19 +0200

    implemented search for latest binary; added jest framework and test

---
+++ src/binary.provider.spec.ts
@@ -0,0 +1,42 @@
+import { promises as fs } from 'fs';
+import { findBinaryForUpdate } from './binary.provider';
+
+const mockBinaryNames = [
+  'firmware-v10-foobar1.bin',
+  'firmware-v1-barf002.bin',
+  'firmware-v100-baz4bar.bin',
+  'firmware-v42-boo0000.bin',
+  'firmware-v20-9876boz.bin'
+];
+
+test('should throw an error if chipId not found', async () => {
+  await expect(findBinaryForUpdate('invalid')).rejects.toThrow();
+});
+
+test('should return the binary path', async () => {
+  spyOn(fs, 'readdir').and.returnValue(['firmware-v1-foobar1.bin']);
+
+  const files = await findBinaryForUpdate('123456');
+  expect(files).toMatch(/firmware-v1-foobar1.bin/);
+});
+
+test('should return undefined if no newer version available', async () => {
+  spyOn(fs, 'readdir').and.returnValue(mockBinaryNames);
+
+  const files = await findBinaryForUpdate('123456', 'v100-baz4bar');
+  expect(files).toBe(undefined);
+});
+
+test('should return latest version if no version given', async () => {
+  spyOn(fs, 'readdir').and.returnValue(mockBinaryNames);
+
+  const files = await findBinaryForUpdate('123456');
+  expect(files).toMatch(/firmware-v100-baz4bar.bin/);
+});
+
+test('should return latest version if a newer one is available', async () => {
+  spyOn(fs, 'readdir').and.returnValue(mockBinaryNames);
+
+  const files = await findBinaryForUpdate('123456', 'v20-9876boz');
+  expect(files).toMatch(/firmware-v100-baz4bar.bin/);
+});
---
+++ src/binary.provider.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+import { promises as fs } from 'fs';
+
+export const findBinaryForUpdate = async (chipId: string, currentVersion?: string): Promise<string | undefined> => {
+  const chipFolderPath = path.resolve(__dirname, '..', 'binfiles', chipId);
+  try {
+    const allBinaries = (await fs.readdir(chipFolderPath)).filter(fileName => fileName.match(/\.bin$/));
+    const fileNameOfLatestVersion = getFileNameOfLatestVersion(allBinaries, currentVersion);
+    if (fileNameOfLatestVersion) {
+      return path.resolve(chipFolderPath, fileNameOfLatestVersion);
+    }
+  } catch (err) {
+    throw new Error(`Error reading directory ${chipFolderPath}: ${err}`);
+  }
+};
+
+export const getFileNameOfLatestVersion = (files: string[], currentVersion?: string): string | undefined => {
+  if (!files || files.length === 0) {
+    return undefined;
+  }
+
+  // sort by version
+  files.sort((a, b) => {
+    const versionAMatches = a.match(/-v(\d+)-/);
+    const versionBMatches = b.match(/-v(\d+)-/);
+    if (!versionAMatches || !versionBMatches) {
+      throw new Error(`Cannot not parse version during sorting.`);
+    }
+    const versionA = +versionAMatches[1];
+    const versionB = +versionBMatches[1];
+    return versionA - versionB;
+  });
+
+  const fileNameWithLatestVersion: string = files[files.length - 1];
+  const latestVersionMatches = fileNameWithLatestVersion.match(/-(v\d+-[a-z0-9]{7})/); // e.g. v1-foobar1, v42-baz2bar, ...
+  if (!latestVersionMatches || latestVersionMatches.length < 2) {
+    throw new Error(`Cannot parse version from name "${fileNameWithLatestVersion}"`);
+  }
+  const latestVersion: string = latestVersionMatches[1];
+
+  if (!currentVersion || currentVersion !== latestVersion) {
+    return fileNameWithLatestVersion;
+  }
+};
